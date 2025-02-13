877970a89003e3c2b8ed52bf4c714b5a0dcdc823
omnema007 <nemaom2003@gmail.com>
2025-02-13 15:09:04 +0530

    [Om] Add. Solution of Implement a Stack Using Queues

---
+++ Day02/src/main/java/collectionframework/queueinterface/stackusingqueue/StackUsingQueueMain.java
@@ -0,0 +1,11 @@
+package collectionframework.queueinterface.stackusingqueue;
+
+public class StackUsingQueueMain {
+    public static void main(String[] args) {
+       StackUsingQueues.push(1);
+       StackUsingQueues.push(2);
+       StackUsingQueues.push(3);
+
+        System.out.println(StackUsingQueues.pop());
+    }
+}
