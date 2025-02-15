ec0d9882221563f5760ac278c7a74aa8f6610098
omnema007 <nemaom2003@gmail.com>
2025-02-15 23:13:49 +0530

    [Om] Add. Solution of  Testing File Handling Methods

---
+++ Day05/src/main/java/junitproblems/basicjunitproblems/testingfilehandlingmethods/FileHandling.java
@@ -0,0 +1,22 @@
+package junitproblems.basicjunitproblems.testingfilehandlingmethods;
+import java.io.*;
+
+public class FileHandling {
+    public static void writeToFile(String filename, String content) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(content);
+        }
+    }
+
+    public static String readFromFile(String filename) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        }
+        return content.toString();
+    }
+}
+
